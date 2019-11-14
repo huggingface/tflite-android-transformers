f60f9ee2116458a6f937a3450ffc72317f753256
Pierric Cistac <pierric@huggingface.co>
2019-11-14 18:58:04 -0500

    add model generation files

---
+++ models_generation/distilbert.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from transformers import TFDistilBertForQuestionAnswering
+
+model = TFDistilBertForQuestionAnswering.from_pretrained('distilbert-base-uncased-distilled-squad')
+
+input_spec = tf.TensorSpec([1, 384], tf.int32)
+model._set_inputs(input_spec, training=False)
+
+print(model.inputs)
+print(model.outputs)
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
+
+tflite_model = converter.convert()
+
+open("distilbert-squad-384.tflite", "wb").write(tflite_model)
